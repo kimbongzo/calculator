f32d0a7f0e6ae64430778c507698841508008af6
kimbongzo <kimbongzo@gmail.com>
2015-07-09 09:19:30 +0900

    fixed #1스켈레톤 작성

---
+++ calcuexammain.c
@@ -0,0 +1,21 @@
+#include <studio.h>
+
+int add(int, int);
+int sub(int, int);
+int div(int, int);
+int mul(int, int);
+int main(int argc, char* argv[])
+{
+    int a, b, c;
+    char operator;
+    
+    printf("Please enter a expression");
+    scanf("%d %c %d", &a, &operator, &b);
+    
+    switch(operator)
+    {
+        case '+':
+            c = add(a, b);
+            break;
+    
+}
